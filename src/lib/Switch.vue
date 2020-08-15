b37c9555dab84e3b1c13380d10811f684304e521
JamesLHY <84779512@qq.com>
2020-08-15 13:39:45 +0800

    优化css代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -7,11 +7,11 @@
 <script lang="ts">
 
     export default {
-        props:{
-            value:Boolean
+        props: {
+            value: Boolean
         },
-        setup(props,context){
-            const toggle = ()=>{
+        setup(props, context) {
+            const toggle = () => {
                 context.emit('update:value', !props.value)
             }
             return {toggle}
@@ -28,24 +28,30 @@
         background: grey;
         border-radius: $h/2;
         position: relative;
+
+        > span {
+            position: absolute;
+            top: 2px;
+            left: calc(100% - #{$h2} - 2px);
+            height: $h2;
+            width: $h2;
+            background: white;
+            border-radius: $h2/2;
+            transition: left .5s;
+        }
+        &.checked {
+            background: blue;
+
+            > span {
+                left: 2px;
+            }
+        }
+
+        &:focus {
+            outline: none;
+        }
     }
-    button.checked{
-        background: blue;
-    }
-    button.checked > span{
-        left: 2px;
-    }
-    button:focus{
-        outline: none;
-    }
-    span {
-        position: absolute;
-        top: 2px;
-        left: calc(100% - #{$h2} - 2px);
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2/2;
-        transition: left .5s;
-    }
+
+
+
 </style>
