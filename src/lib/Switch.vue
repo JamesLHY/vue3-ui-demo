f15c06140b3f924acc4c1abff71f14cde58ab208
JamesLHY <84779512@qq.com>
2020-08-15 15:50:36 +0800

    完成Button基本样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -18,7 +18,7 @@
         }
     }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
     $h: 22px;
     $h2: $h - 4px;
     button {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 
 import { createApp } from "vue";
-import App from "./App.vue";
+import App from './App.vue';
 import "./index.scss";
 import { router } from './router';
 
