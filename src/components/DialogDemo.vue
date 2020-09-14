8a9c44d2faf7731be1988f471fc99fa2c20088f7
JamesLHY <84779512@qq.com>
2020-09-14 11:43:31 +0800

    visible的设计

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,19 +1,26 @@
 <template>
-    <div>
-        dialog的文档
-        <h1>示例一
-            
-        </h1>
-        <Dialog></Dialog>
-
-    </div>
+  <div>
+    dialog的文档
+    <h1>示例一</h1>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :visible="x"></Dialog>
+  </div>
 </template>
 
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
 export default {
-components:{Dialog}
-}
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle =()=>{
+        x.value=!x.value
+    }
+    return { x ,toggle};
+  },
+};
 </script>
 
