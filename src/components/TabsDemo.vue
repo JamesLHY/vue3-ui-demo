fc681072b8f4e6bb98430e16b796048b5f14f229
JamesLHY <84779512@qq.com>
2020-09-22 11:18:03 +0800

    openDialog一句话打开

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
         tabsDemo的文档
-
     </div>
+    <h1>示例一</h1>
+    <Tabs>
+        <Tab>tab1</Tab>
+        <Tab>tab2</Tab>
+    </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+
+export default {
+components:{Tabs,Tab}
+}
+</script>
+
